383087f23829d4aad0b21c21a3f4b1b861e6e537
sergiogomezsantos <sergiogs2003@gmail.com>
2026-02-10 20:34:10 +0100

    ...

--- pkg/agent/collector.go
+++ pkg/agent/collector.go
@@ -11,32 +11,32 @@ import (
 
 // GetMetrics se encargará de obtener la información necesaria de los equipos que tengan el agente instalado
 func GetMetrics() (platform.SystemStats, error) {
-    // 1. Conseguir el hostname y el OS mediante el uso del paquete host y la función Info
+    // Conseguir el hostname y el OS mediante el uso del paquete host y la función Info
     hostInfo, err := host.Info()
     if err != nil {
         // Si falla se devuelve la estructura vacía y el error, así con todos los campos
         return platform.SystemStats{}, err
     }
 
-    // 2. Conseguir el uso de la CPU
+    // 1. Conseguir el uso de la CPU
     cpuUsagePercent, err := cpu.Percent(0, false)
     if err != nil {
         return platform.SystemStats{}, err
     }
 
-    // 3. Conseguir el uso de la RAM
+    // 2. Conseguir el uso de la RAM
     ramUsagePercent, err := mem.VirtualMemory()
     if err != nil {
         return platform.SystemStats{}, err
     }
 
-    // 4. Conseguir el uso del disco
+    // 3. Conseguir el uso del disco
     diskUsagePercent, err := disk.Usage("/")
     if err != nil {
         return platform.SystemStats{}, err
     }
 
-    // 5. Rellenar el struct SystemStats con los datos obtenidos
+    // 4. Rellenar el struct SystemStats con los datos obtenidos
     var stats platform.SystemStats
     stats.Hostname = hostInfo.Hostname
     stats.Platform = hostInfo.Platform
