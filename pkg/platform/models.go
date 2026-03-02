45796beb0889d99d51e1767fcae71784377a0466
sergiogomezsantos <sergiogs2003@gmail.com>
2026-03-02 16:35:01 +0100

    intento de creación de alertas, próximo día serguir con ello. De momento sólo el struct creado, queda guardar las alertas en la BD y establecer cuándo un SystemStats se conidera alerta en el archivo alerts.go

--- pkg/platform/models.go
+++ pkg/platform/models.go
@@ -1,5 +1,14 @@
 package platform
 
+// Tipo de Alerta
+type AlertType string
+
+const (
+    CPUAlert AlertType = "CPU_HIGH_USAGE"
+    RAMAlert AlertType = "RAM_HIGH_USAGE"
+    DISKAlert AlertType = "DISK_HIGH_USAGE"
+)
+
 // SystemStats define la información que los agentes instalados en los distintos equipo van a enviar al servidor principal
 type SystemStats struct {
     ID uint `json:"id" gorm:"primaryKey"` // ID del struct
@@ -12,4 +21,17 @@ type SystemStats struct {
     OutboundNetTraffic int64 `json:"outbound_net_traffic"` // Tráfico de red saliente
     Processes string `json:"processes"` // Top 5 procesos que más están consumiendo
     Time int64 `json:"time"` // Cuándo se generó el informe
+}
+
+// Alert define la información que el servidor enviará a los usuarios para notificar cualquier irregularidad detectada en los equipos por el agente
+type Alert struct {
+    ID uint    `json:"id" gorm:"primaryKey"` // ID de la alerta
+    Time int64   `json:"time"` // Cuándo ocurrió la alerta
+    Hostname string `json:"hostname" gorm:"index"` // Nombre del equipo
+    Type AlertType `json:"type" gorm:"index;type:text"` // Tipo de alerta
+    Value float64 `json:"value"` // Valor de la alerta
+    Threshold float64 `json:"threshold"` // Umbral de la alerta
+    Severity uint8 `json:"severity"` // Severidad de la alerta (Info, Warning o Critical)
+    SystemStatsID uint `json:"system_stats_id" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // ID de las estadísticas del equipo
+    SystemStats SystemStats `json:"system_stats"` // Información de las estadísticas del equipo
 }
--- pkg/server/alerts.go
+++ pkg/server/alerts.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+    "github.com/AressS-Git/syspulse/pkg/platform"
+    "fmt"
+    "time"
+)
+
+// Constantes que servirán cómo umbrales para decidir si algo es una alerta o no lo es
+const (
+    CPU_THRESHOLD float64 = 95.0
+    RAM_THRESHOLD float64 = 95.0
+    DISK_THRESHOLD float64 = 90.0
+)
+
+// CreateAlert creará una alerta si las estadísticas de la CPU, RAM y Disco superan los ciertos límites
+func CreateAlert(stats platform.SystemStats) {
+    // Crear alerta si la CPU supera el umbral
+    if(stats.CpuUsage > CPU_THRESHOLD) {
+        alert := platform.Alert {
+            Time: time.Now().Unix(),
+            Hostname: stats.Hostname,
+            Type: platform.CPUAlert,
+            Value: stats.CpuUsage,
+            Threshold: CPU_THRESHOLD,
+            Severity: 2,
+            SystemStatsID: stats.ID,
+            SystemStats: stats,
+        }
+    }
+}
