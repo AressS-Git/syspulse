80b7dff3b82388804d9646f0e99abe38cab59199
sergiogomezsantos <sergiogs2003@gmail.com>
2026-03-15 13:40:07 +0100

    ...

--- cmd/pulse-agent/main.go
+++ cmd/pulse-agent/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
     // Definimos la URL donde está escuchando nuestro servidor
-    serverURL := "http://MacBook-Air-de-Sergio.local:8080/api/stats"
+    serverURL := "http://192.168.1.14:8080/api/stats"
 
     fmt.Println("Iniciando Agente SysPulse...")
 
